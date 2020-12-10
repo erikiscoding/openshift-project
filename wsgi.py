8810822f43c446c5d051462fb683f9fd7566de32
Erik Lewis <61423542+erikiscoding@users.noreply.github.com>
2020-12-10 18:20:20 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,12 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    your_word = str(input("Enter what word you really really like and enjoy greatly: "))
-
-    your_word = your_word.upper()
-
-    print("Here is your word but even better: " + your_word)
-    print("bye bye now")
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.debug = True
