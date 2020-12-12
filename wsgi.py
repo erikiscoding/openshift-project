846bd02c5b596d055a77a60d6b7da18f1a12665b
Erik Lewis <leeeriklewis@gmail.com>
2020-12-11 23:17:29 -0500

    finally got this to work

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_wtf import FlaskForm
+
 application = Flask(__name__)
 
 
@@ -6,6 +8,17 @@ application = Flask(__name__)
 @application.route("/home")
 def home():
     return render_template('layout.html')
+
+
+@application.route("/",methods=['GET','POST'])
+@application.route("/home",methods=['GET','POST'])
+def conversion():
+    #submission = request.args.get('currencies')
+    submission = request.form['currencies']
+    return str(submission)
+
+
+
 
 
 if __name__ == "__main__":
